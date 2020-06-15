989c6430b7c334d18709c974898f72ef3f9acd31
alifananda <alif.ananda33@gmail.com>
2020-06-15 19:06:23 +0700

    update

--- modul/laporanpresensi.php
+++ modul/laporanpresensi.php
@@ -5,7 +5,7 @@
    $nama = $_SESSION['username'];
    $user_level = $_SESSION['level'];
   if(!isset($_SESSION['username'])){
-  
+
     header('location:login.php');
   }
 
@@ -20,12 +20,12 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="card-box card shadow">
-                                        
+
                                         <div class="card-body">
 
                                           <h2 class="mb-0">Laporan Data Presensi</h2>
                                            <hr/>
-                                        <!-- 
+                                        <!--
                                            <button type="button" class="btn btn-sm btn-primary mt-1 mb-1" data-toggle="modal" data-target="#largeModal">Export Data Presensi</button><br/><br/> -->
 
 
@@ -42,7 +42,7 @@
 
                                    <th class="wd-15p">Tipe </th>
 
-                             
+
                          <!-- <th class="wd-10p">Aksi</th> -->
                             </tr>
                           </thead>
@@ -53,12 +53,12 @@
                             $no;
                             while ($q=mysql_fetch_array($query)) {
                               $no++;
-                             
+
 
                              echo "
                               <tr>
                               <td>$no</td>
-                               
+
                                 <td>$q[id_user]</td>
                                 <td>$q[Hari] </td>";
                                 if ($q[PresensiMasuk]=="") {
@@ -71,7 +71,7 @@
                                 }else{
                                   $kel=$q[PresensiKeluar];
                                 }
-                               
+
 
                                echo" <td><span class=\"badge badge-success\">$pre </span></td>
                                 <td><span class=\"badge badge-success\">$kel </span></td>
@@ -79,13 +79,13 @@
                                  <td>$q[Tipe] </td>
                                   </tr>";
 
-                              
+
                             }
 
 
                             ?>
-                        
-                          
+
+
                           </tbody>
                         </table>
                 </div>
@@ -96,11 +96,11 @@
   case 'edit':
          $edit=mysql_fetch_assoc(mysql_query("select * from jadwal where id='$_GET[id]'"));
 
-     
+
 
         ?>
 
-        
+
 
 
         <?php
