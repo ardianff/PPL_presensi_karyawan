97609c271ab3282936411be2584411ea33371a19
ardianff <ardianfirmansyah123@gmail.com>
2020-07-09 20:23:49 +0700

    pindah repo

--- android/PresensiFaceGeofencing/app/src/main/java/com/example/presensifacegeofencing/config/Server.java
+++
@@ -1,9 +0,0 @@
-package com.example.presensifacegeofencing.config;
-
-public class Server {
-    public static final String URL = "http://septian.jstmade.com/";
-    public static final String URL_PHOTO = "http://septian.jstmade.com/web_service/";
-
-
-
-}
